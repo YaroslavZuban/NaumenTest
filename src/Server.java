ba43e222b37578b6f44f664adc697ccb3c81eabd
YaroslavZuban <yaroslav.zuban7029@gmail.com>
2022-05-09 14:24:09 +0700

    Рабочий код

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.net.CacheRequest;
 
 public class Server {
     public static void main(String[] args) {
--- src/СacheServer.java
+++ src/СacheServer.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.net.CacheRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
